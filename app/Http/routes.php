132bcd75cc0115a4797d46a22427d74672dee667
Aran Gilmore <aran@arangilmore.com>
2015-10-19 16:21:23 -0400

    update routes and TagController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/practice', function(){
-	 echo App::environment();
+	echo config('app.url');
 });
 Route::resource('tag', 'TagController');
