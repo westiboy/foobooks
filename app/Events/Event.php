81ef1e02d45d2e232fccf43c03d5b8f48a85c1bf
Aran Gilmore <aran@arangilmore.com>
2015-10-19 14:28:33 -0400

    First Commit2

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Foobooks\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Foobooks\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
